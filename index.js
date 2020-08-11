59eddc6ae3c314b3496e7bad0077baefa756e2ee
Steve Anderson <prototype909@msn.com>
2020-08-11 22:23:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,20 +7,20 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
   driver[key] = value;
-  
+
   return driver;
 }
 
 function deleteFromDriverByKey(driver, key, value) {
   const newObject = Object.assign({}, driver);
-  
+
   delete newObject[key];
-  
+
   return newObject;
 }
 
 function destructivelyDeleteFromDriverByKey(driver, key) {
   delete driver[key];
-  
+
   return driver;
 }
